de475183d8b2b0e2fd51d7b1fdd653b62cc2eb11
50512 <me@50512.dev>
2026-09-18 12:17:11 -0500

    add checkboxes

---
+++ src/components/inputs/index.ts
@@ -0,0 +1,4 @@
+export { default as LabButton } from "./LabButton.astro";
+export { default as LabTextInput } from "./LabInput.astro";
+export { default as LabCheckbox } from "./LabCheckbox.astro";
+export { default as LabToggle } from "./LabToggle.astro";
